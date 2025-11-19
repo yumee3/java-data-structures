39afe6f30410e1b4166abb9f16b95a7fab3cd368
Muzamel Pupal <muzamel.1203@gmail.com>
2025-11-19 01:09:53 +0100

    Add DoublyLinkedList interface

--- src/datastructures/interfaces/IDoublyLinkedList.java
+++ src/datastructures/interfaces/IDoublyLinkedList.java
@@ -1,4 +1,44 @@
 package datastructures.interfaces;
 
-public interface IDoublyLinkedList {
+/**
+ * Interface defining the basic operations of a doubly linked list.
+ *
+ * @param <E> the type of elements stored in the list
+ */
+public interface IDoublyLinkedList<E> {
+
+    /**
+     * Inserts an element at the beginning of the list.
+     *
+     * @param e the element to add
+     */
+    void addFirst(E e);
+
+    /**
+     * Inserts an element at the end of the list.
+     *
+     * @param e the element to add
+     */
+    void addLast(E e);
+
+    /**
+     * Removes and returns the first element of the list.
+     *
+     * @return the removed element
+     */
+    E removeFirst();
+
+    /**
+     * Removes and returns the last element of the list.
+     *
+     * @return the removed element
+     */
+    E removeLast();
+
+    /**
+     * Checks whether the list is empty.
+     *
+     * @return true if empty, false otherwise
+     */
+    boolean isEmpty();
 }
